096457151a8f07ea79cd8ef5ebc102360df5b519
DoubleWoot <202110423@feualabang.edu.ph>
2025-02-18 16:52:36 +0800

    Fixed around the margin and paddings of each div to prepare for the overhaul of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,8 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div className="my-12 mx-72 py-4 px-4 place-self-center">
+
+      <div className="py-12 px-72 place-self-center w-full">
         <p className="font-trap text-4xl text-center">
           <span className="font-bold">The Blockchain Campus Conference</span> is
           a free 2-day nationwide campus conference series designed to tackle
@@ -80,61 +81,12 @@ export default function Home() {
         </p>
       </div>
 
-      {/* Image Carousel */}
-      <div className="relative my-12 mx-auto max-w-3xl overflow-hidden">
-        <div
-          className="flex transition-transform duration-500"
-          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-        >
-          {bcc_images.map((image, index) => (
-            <img
-              key={index}
-              className="w-full h-96 object-cover object-center aspect-[3/4] rounded-lg flex-shrink-0"
-              src={image}
-              alt={`BCC 2024 ${index + 1}`}
-              draggable="false"
-            />
-          ))}
-        </div>
-        {/* Left Button */}
-        <button
-          onClick={prevSlide}
-          className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-800/75 p-2 rounded-full text-white hover:bg-gray-600"
-        >
-          <ChevronLeft size={32} />
-        </button>
-
-        {/* Right Button */}
-        <button
-          onClick={nextSlide}
-          className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-800/75 p-2 rounded-full text-white hover:bg-gray-600"
-        >
-          <ChevronRight size={32} />
-        </button>
-      </div>
-      <div className="flex justify-center space-x-16 mb-12">
-        <a
-          href="https://drive.google.com/drive/folders/1QIc8MWpwmBHxtYUixldjByeUswsHnJZw"
-          target="_blank"
-          rel="noopener"
-        >
-          <button className="p-4 bg-[#E9E9E9] text-[#0F1728] font-mono font-bold  text-3xl rounded-lg">
-            Day 1
-          </button>
-        </a>
-        <a
-          href="https://drive.google.com/drive/folders/1pzB4nGSJq8OmLLxSJbSCP0kbeO8p4ZwA"
-          target="_blank"
-          rel="noopener"
-        >
-          <button className="p-4 bg-[#E9E9E9] text-[#0F1728] font-mono font-bold  text-3xl rounded-lg">
-            Day 2
-          </button>
-        </a>
-      </div>
-      <div className="my-12 mx-72 py-4 px-4 place-self-center">
+      <div className="py-12 px-4 w-full place-self-center">
+        <h1 className="font-trap font-bold text-3xl text-center">
+          Blockchain Campus Conference 2024 Recap
+        </h1>
         <iframe
-          className="rounded-lg"
+          className="rounded-lg mx-auto pt-6"
           width="1280"
           height="720"
           src="https://www.youtube.com/embed/ZHGzJjPRQog?si=uK496HWK77DXX6bw"
@@ -143,6 +95,63 @@ export default function Home() {
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
         ></iframe>
+      </div>
+
+      {/* Image Carousel */}
+      <div className="w-full py-12">
+        <div className="relative mx-auto max-w-3xl overflow-hidden">
+          <div
+            className="flex transition-transform duration-500"
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          >
+            {bcc_images.map((image, index) => (
+              <img
+                key={index}
+                className="w-full h-96 object-cover object-center aspect-[3/4] rounded-lg flex-shrink-0"
+                src={image}
+                alt={`BCC 2024 ${index + 1}`}
+                draggable="false"
+              />
+            ))}
+          </div>
+
+          {/* Left Button */}
+          <button
+            onClick={prevSlide}
+            className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-800/75 p-2 rounded-full text-white hover:bg-gray-600"
+          >
+            <ChevronLeft size={32} />
+          </button>
+
+          {/* Right Button */}
+          <button
+            onClick={nextSlide}
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-800/75 p-2 rounded-full text-white hover:bg-gray-600"
+          >
+            <ChevronRight size={32} />
+          </button>
+
+          <div className="flex justify-center space-x-16 mt-6">
+            <a
+              href="https://drive.google.com/drive/folders/1QIc8MWpwmBHxtYUixldjByeUswsHnJZw"
+              target="_blank"
+              rel="noopener"
+            >
+              <button className="p-4 bg-[#E9E9E9] text-[#0F1728] font-mono font-bold  text-3xl rounded-lg">
+                Day 1
+              </button>
+            </a>
+            <a
+              href="https://drive.google.com/drive/folders/1pzB4nGSJq8OmLLxSJbSCP0kbeO8p4ZwA"
+              target="_blank"
+              rel="noopener"
+            >
+              <button className="p-4 bg-[#E9E9E9] text-[#0F1728] font-mono font-bold  text-3xl rounded-lg">
+                Day 2
+              </button>
+            </a>
+          </div>
+        </div>
       </div>
 
       {/* TODO:  Add the entire list for partners & sponsors */}
