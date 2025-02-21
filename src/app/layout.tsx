8159ffe8676fd120c138bd00949c77c43719a027
DoubleWoot <202110423@feualabang.edu.ph>
2025-02-21 16:40:44 +0800

    Removed the declaration of Metadata inside layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import bcc_logo from "../../public/bg-assets/bcc-logo.png";
